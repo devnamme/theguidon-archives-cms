33c5235764829f9ad724bcc523ff77cdb7e58614
Emman Evangelista <emmanuel.evangelista@student.ateneo.edu>
2024-05-29 15:24:34 +0800

    fix endpoints

--- functions.php
+++ functions.php
@@ -11,62 +11,97 @@ include 'functions/custom-meta.php';
 add_theme_support('admin-bar', array( 'callback' => '__return_false'));
 
 
-function archivescms_get_articles() {
-  $articles = array();
+function archivescms_issue_response($query) {
+  $categories = array();
+  $categories_raw = get_the_category();
+  foreach ($categories_raw as $categ) {
+    $categories[] = $categ->slug;
+  }
 
-  $query = new WP_Query(array(
+  return array(
+    'fixed_slug' => get_post_meta($query->post->ID, 'fixed_slug', true),
+    'title' => get_the_title(),
+    'date_published' => get_the_date('c'),
+    'description' => get_post_meta($query->post->ID, 'description', true),
+    'is_legacy' => get_post_meta($query->post->ID, 'is_legacy', true) == "true" ? true : false,
+    'num_pages' => get_post_meta($query->post->ID, 'num_pages', true),
+    'shortlink' => get_post_meta($query->post->ID, 'shortlink', true),
+    'cover' => wp_get_attachment_url(get_post_thumbnail_id($query->post->ID)),
+    'full_issue' => get_post_meta($query->post->ID, 'full_issue', true),
+    'content' => get_post_meta($query->post->ID, 'content', true),
+    'categories' => $categories,
+  );
+}
+
+
+function archivescms_get_issues($req) {
+  $args = array(
     'posts_per_page' => 20,
-  ));
+  );
+
+  $categ = $req->get_param('categ');
+  if (isset($categ))
+    $args['category_name'] = $categ;
+
+  $page = $req->get_param('page');
+  if (isset($page))
+    $args['paged'] = intval($page);
+
+
+  $query = new WP_Query($args);
+  $issues = array();
 
   while ($query->have_posts()) {
     $query->the_post();
-    
-    $bylines = array();
-    $bylines_raw = array_values(array_filter(array_map(
-      fn($val): string => trim($val),
-      preg_split("/\r\n|\n|\r/", get_post_meta($query->post->ID, 'bylines', true)),
-    )));
-
-    for ($i = 0; $i < count($bylines_raw); $i++) {
-      if ($i % 2 == 1 || $i + 1 >= count($bylines_raw)) continue;
-
-      $bylines[] = array(
-        'key' => $bylines_raw[$i],
-        'value' => $bylines_raw[$i + 1],
-      );
-    }
-
-    $categories = array();
-    $categories_raw = get_the_category();
-    foreach ($categories_raw as $categ) {
-      $categories[] = $categ->slug;
-    }
-
-    $articles[] = array(
-      'fixed_slug' => get_post_meta($query->post->ID, 'fixed_slug', true),
-      'title' => get_the_title(),
-      'date_published' => get_the_date('c'),
-      'description' => get_post_meta($query->post->ID, 'description', true),
-      'is_legacy' => get_post_meta($query->post->ID, 'is_legacy', true) == "true" ? true : false,
-      'num_pages' => get_post_meta($query->post->ID, 'num_pages', true),
-      'shortlink' => get_post_meta($query->post->ID, 'shortlink', true),
-      'cover' => wp_get_attachment_url(get_post_thumbnail_id($query->post->ID)),
-      'full_issue' => get_post_meta($query->post->ID, 'full_issue', true),
-      'content' => get_post_meta($query->post->ID, 'content', true),
-      'categories' => $categories,
-    );
+    $issues[] = archivescms_issue_response($query);
   }
-
   wp_reset_postdata();
 
-  return rest_ensure_response($articles);
+  return rest_ensure_response(array(
+    'max_pages' => $query->max_num_pages,
+    'found' => $query->found_posts,
+    'issues' => $issues,
+  ));
+}
+
+
+function archivescms_get_issue($req) {
+  $issue = array();
+
+  $query = new WP_Query(array(
+    'meta_query' => array(
+      array('key' => 'fixed_slug', 'value' => $req['slug']),
+    ),
+  ));
+
+  if ($query->have_posts()) {
+    $query->the_post();
+    $issue = archivescms_issue_response($query);
+  }
+  wp_reset_postdata();
+
+  return rest_ensure_response($issue);
 }
 
 
 add_action('rest_api_init', function() {
+  /**
+   * GET /issues
+   * Get latest issues
+   * ?categ
+   * ?page
+   */
   register_rest_route('api/v1', 'issues', array(
       'methods' => 'GET',
-      'callback' => 'archivescms_get_articles'
+      'callback' => 'archivescms_get_issues'
+  ));
+
+  /**
+   * GET /issue/:slug
+   */
+  register_rest_route('api/v1', 'issue/(?P<slug>[a-zA-Z0-9-_]+)', array(
+    'methods' => 'GET',
+    'callback' => 'archivescms_get_issue',
   ));
 });
 
