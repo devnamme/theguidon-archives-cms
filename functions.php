c6f4f221da16ce3a1b0340e9e153de5eb1112c77
Emman Evangelista <emmanuel.evangelista@student.ateneo.edu>
2024-05-30 19:24:23 +0800

    fixes for paged

--- functions.php
+++ functions.php
@@ -6,6 +6,28 @@ include 'functions/custom-meta.php';
 
 
 /**
+ * CORS
+ */
+// function add_cors_http_header() {
+//   header("Access-Control-Allow-Origin: *");
+// }
+
+function init_cors( $value ) {
+$origin = get_http_origin();
+$allowed_origins = ['theguidon.github.io', 'localhost:5173', 'dev.theguidon.com', 'archives.theguidon.com'];
+
+if ( $origin && in_array( $origin, $allowed_origins ) ) {
+  header( 'Access-Control-Allow-Origin: ' . esc_url_raw( $origin ) );
+  header( 'Access-Control-Allow-Methods: GET' );
+  header( 'Access-Control-Allow-Credentials: true' );
+}
+
+return $value;
+}
+// add_action('init', 'init_cors');
+
+
+/**
  * Disables WordPress admin bar
  */
 add_theme_support('admin-bar', array( 'callback' => '__return_false'));
@@ -24,7 +46,7 @@ function archivescms_issue_response($query) {
     'date_published' => get_the_date('c'),
     'description' => get_post_meta($query->post->ID, 'description', true),
     'is_legacy' => get_post_meta($query->post->ID, 'is_legacy', true) == "true" ? true : false,
-    'num_pages' => get_post_meta($query->post->ID, 'num_pages', true),
+    'num_pages' => intval(get_post_meta($query->post->ID, 'num_pages', true)),
     'shortlink' => get_post_meta($query->post->ID, 'shortlink', true),
     'cover' => wp_get_attachment_url(get_post_thumbnail_id($query->post->ID)),
     'full_issue' => get_post_meta($query->post->ID, 'full_issue', true),
@@ -44,9 +66,7 @@ function archivescms_get_issues($req) {
     $args['category_name'] = $categ;
 
   $page = $req->get_param('page');
-  if (isset($page))
-    $args['paged'] = intval($page);
-
+  $args['paged'] = isset($page) ? intval($page) : 1;
 
   $query = new WP_Query($args);
   $issues = array();
@@ -58,7 +78,7 @@ function archivescms_get_issues($req) {
   wp_reset_postdata();
 
   return rest_ensure_response(array(
-    'page' => intval($page),
+    'page' => isset($page) ? intval($page) : 1,
     'max_pages' => $query->max_num_pages,
     'categ' => $categ,
     'found' => $query->found_posts,
@@ -90,8 +110,12 @@ add_action('rest_api_init', function() {
   /**
    * GET /issues
    * Get latest issues
+   * ?search
    * ?categ
    * ?page
+   * ?from
+   * ?to
+   * ?sort
    */
   register_rest_route('api/v1', 'issues', array(
       'methods' => 'GET',
@@ -105,6 +129,20 @@ add_action('rest_api_init', function() {
     'methods' => 'GET',
     'callback' => 'archivescms_get_issue',
   ));
+
+  /**
+   * GET /legacy
+   * ?categ
+   * ?page
+   */
+  // register_rest_route('api/v1', 'legacy', array(
+  //   'methods' => 'GET',
+  //   'callback' => 'archivescms_get_legacy',
+  // ));
+
+  /**
+   * GET /search
+   */
 });
 
 
